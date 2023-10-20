2efb4848d90fca5c7e0412504c7281a4615a0096
vishruth555 <vishruth555@gmail.com>
2023-10-20 13:26:51 +0530

    inital commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Footer extends React.Component{
+    render() {
+        return (
+            <footer>
+                <small>
+                    <a href="https://github.com/vishruth555">github</a>
+                </small>
+            </footer>
+        )
+    }
+}
+
+export default Footer
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+import Main from './components/Main';
+
+render(
+  <Main />, 
+  document.getElementById("root")
+);
